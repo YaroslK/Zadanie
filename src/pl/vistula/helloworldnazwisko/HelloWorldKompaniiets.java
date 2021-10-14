199285632f3e2587173e3873cf9d62d3cfee7c81
YaroslK <ggwpzemlya@gmail.com>
2021-10-14 22:53:21 +0300

    Initial Comit: Basic structure and simple classes.

---
+++ src/pl/vistula/helloworldnazwisko/HelloWorldKompaniiets.java
@@ -0,0 +1,8 @@
+package pl.vistula.helloworldnazwisko;
+
+public class HelloWorldKompaniiets {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistual Kompaniiets 54034!");
+        System.out.println("Hello World!");
+    }
+}
