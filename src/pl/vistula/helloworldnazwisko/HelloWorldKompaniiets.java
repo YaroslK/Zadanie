e5d30604352fe2d9e82eeb1e695a11e475e4bf56
YaroslK <92547365+YaroslK@users.noreply.github.com>
2021-10-14 23:17:27 +0300

    Update HelloWorldKompaniiets.java

--- src/pl/vistula/helloworldnazwisko/HelloWorldKompaniiets.java
+++ src/pl/vistula/helloworldnazwisko/HelloWorldKompaniiets.java
@@ -2,7 +2,7 @@ package pl.vistula.helloworldnazwisko;
 
 public class HelloWorldKompaniiets {
     public static void main(String[] args) {
-        System.out.println("Hello Vistual Kompaniiets 54034!");
+        System.out.println("Hello Vistula Kompaniiets 54034!");
         System.out.println("Hello World!");
     }
 }
